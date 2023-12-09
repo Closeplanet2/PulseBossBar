fa747558445ef1c25dd4c90e9d9de6dc56dd7bb7
closeplanet2 <68482902+closeplanet2@users.noreply.github.com>
2023-12-09 12:28:08 +0000

    Init Commit

---
+++ src/main/java/com/pandapulsestudios/pulsebossbar/PulseBossBar.java
@@ -0,0 +1,17 @@
+package com.pandapulsestudios.pulsebossbar;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PulseBossBar extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
